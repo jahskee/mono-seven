166a3506eb86967c8d06ed424019a64abb1573b8
jahskee <jahskee@yahoo.com>
2021-07-16 14:24:38 -0400

    update

---
+++ next-app/src/pages/_app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import T from 'prop-types';
+import client from './apollo-client';
+
+// pages/_app.js
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+MyApp.propTypes = {
+  Component: T.elementType.isRequired,
+};
+export default MyApp;
