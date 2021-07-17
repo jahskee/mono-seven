4bcffa8ecf221e0a0fd0c9703e1dc4343bd630cf
jahskee <jahskee@yahoo.com>
2021-07-17 18:02:16 -0400

    update

--- next-app/src/pages/home/Home.tsx
+++ next-app/src/pages/home/Home.tsx
@@ -5,8 +5,8 @@ import Container from '@material-ui/core/Container';
 import { gql, useLazyQuery } from '@apollo/client';
 import useStyles from './Home.styles';
 import Pokemon from './Pokemon';
-import AppBar7 from '../../storybook/components/appbar/AppBar7';
-import HeroImage from '../../storybook/components/hero-image/HeroImage';
+import AppBar7 from '../../storybook/components/common/appbar/AppBar7';
+import HeroImage from '../../storybook/components/common/hero-image/HeroImage';
 import Table7 from '../../storybook/components/table7/Table7';
 
 import useReactiveVars from '../../appState';
