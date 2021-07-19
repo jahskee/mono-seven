90bcc8de4df386830e75d1eaf1a3d5e2549ef92e
jahskee <jahskee@yahoo.com>
2021-07-19 00:41:43 -0400

    update

--- next-app/src/storybook/components/PokeDetail/PokeDetail.tsx
+++ next-app/src/storybook/components/PokeDetail/PokeDetail.tsx
@@ -10,89 +10,95 @@ import Paper from "@material-ui/core/Paper";
 const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    color: theme.palette.common.white,
   },
   body: {
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
     "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover
-    }
-  }
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
 }))(TableRow);
-
-function createData(name, calories, fat, carbs) {
-  return { name, calories, fat, carbs };
-}
-
-const rows = [
-  createData("ID", 159, "Order", 24),
-  createData("Height", 237, "XP", 37),
-  createData("Weight", 262),
-];
 
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    paddingLeft: '1rem',
-    paddingBottom: '1rem',
+    display: "flex",
+    paddingLeft: "1rem",
+    paddingBottom: "1rem",
   },
   table: {
     minWidth: 300,
     padding: "5",
-    paddingLeft: '8px',
+    paddingLeft: "8px",
   },
   cell: {
-    padding: "5px"
+    padding: "5px",
   },
   imgRow: {
     display: "flex",
     justifyContent: "flex-end",
-    paddingRight: '0.5rem'
+    paddingRight: "0.5rem",
   },
   img: {
-    width: '40px',
-    height: '40px',
-    padding: '0',
-    marginTop: '1.2rem',    
-  }
+    width: "40px",
+    height: "40px",
+    padding: "0",
+    marginTop: "1.2rem",
+  },
 });
 
 export default function PokeDetail() {
   const classes = useStyles();
-
+  const pokemon = {
+    id: "61",
+    name: "Pokemon1",
+    height: "159",
+    weight: "6.0",
+    order: "24",
+    xp: "4.0",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/61.png",
+  };
   return (
     <div className={classes.root}>
       <div className={classes.imgRow}>
         <img
           className={classes.img}
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/61.png"
+          src={pokemon.image}
         />
       </div>
-
+      
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="customized table">
           <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.name}>
-                <StyledTableCell className={classes.cell} align="left">
-                  <b>{row.name}</b>
-                </StyledTableCell>
-                <StyledTableCell className={classes.cell} align="left">
-                  {row.calories}
-                </StyledTableCell>
-                <StyledTableCell className={classes.cell} align="left">
-                  <b>{row.fat}</b>
-                </StyledTableCell>
-                <StyledTableCell className={classes.cell} align="left">
-                  {row.carbs}
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
+            <StyledTableRow key={pokemon.id}>
+              <StyledTableCell className={classes.cell} align="left">
+                <b>ID</b>
+              </StyledTableCell>
+              <StyledTableCell className={classes.cell} align="left">
+                {pokemon.id}
+              </StyledTableCell>
+            </StyledTableRow>
+            <StyledTableRow key={pokemon.id}>
+              <StyledTableCell className={classes.cell} align="left">
+                <b>Height</b>
+              </StyledTableCell>
+              <StyledTableCell className={classes.cell} align="left">
+                {pokemon.height}
+              </StyledTableCell>
+            </StyledTableRow>
+            <StyledTableRow key={pokemon.id}>
+              <StyledTableCell className={classes.cell} align="left">
+                <b>Weight</b>
+              </StyledTableCell>
+              <StyledTableCell className={classes.cell} align="left">
+                {pokemon.weight}
+              </StyledTableCell>
+            </StyledTableRow>
           </TableBody>
         </Table>
       </TableContainer>
