a9eebfa91234c447c8c9e8e5bacf2568ec7ae4b8
jahskee <jahskee@yahoo.com>
2021-07-22 05:39:34 -0400

    update

--- next-app/src/storybook/components/SearchPanel/SearchPanel.tsx
+++ next-app/src/storybook/components/SearchPanel/SearchPanel.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox/SearchBox";
 import SearchResult from "./components/SearchResult/SearchResult";
 import { makeStyles } from "@material-ui/styles";
-import { Grid } from "@material-ui/core";
+import { Grid, Paper } from "@material-ui/core";
 import DataMocks from "../../_data_mocks/data_mocks";
 import { useSearchKey } from '../../../appState/appState';
 import { useLazyQuery, gql } from "@apollo/client";
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
 });
 
 export const FIND_NAMES = gql`
-  query findNames($name: String!){
+  query FindNames($name: String!){
     findNames(name: $name, limit:10) {
       id
       name
@@ -29,18 +29,14 @@ export const FIND_NAMES = gql`
 `;
 
 function SearchPanel() {
-  const { searchKey, setSearchKey} = useSearchKey();
   const [findNames, { data, loading }] = useLazyQuery(FIND_NAMES);
-  const classes = useStyles();
-  const [pokemons, setPokemons] = useState([]);
-
-  const handleSearchChange = (searchKey, limit = 10)=> {
-    console.log("searchkey", searchKey, "limit", 10)
-
-    findNames({variables: {
-      name: searchKey,
-      limit,
-    }});
+  const handleSearchChange = (searchKey, limit = 20)=> {
+    if(searchKey.length > 1) {
+      findNames({variables: {
+        name: searchKey,
+        limit,
+      }});
+    }
   }
 
   if(loading) return <div>Loading...</div>
@@ -50,7 +46,14 @@ function SearchPanel() {
         <SearchBox handleSearchChange={handleSearchChange}/>
       </Grid>
       <Grid item xs={12} >
-        <SearchResult pokemons={data.findNames} />
+        {data && 
+         <SearchResult pokemons={data.findNames} />
+        }
+        {!data && 
+         <Paper style={{minHeight: '483px'}}>
+
+         </Paper>
+        }
       </Grid>
     </Grid>
   );
