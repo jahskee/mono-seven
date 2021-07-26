8b79b4b292c3440c78c736e684c0de6b11135e2f
jahskee <jahskee@yahoo.com>
2021-07-25 20:02:20 -0400

    stable v0.0.01

--- infra/terraform/aws/logs.tf
+++ infra/terraform/aws/logs.tf
@@ -2,15 +2,15 @@
 
 # Set up CloudWatch group and log stream and retain logs for 30 days
 resource "aws_cloudwatch_log_group" "cb_log_group" {
-  name              = "/ecs/web-app"
+  name              = "/ecs/cb-app"
   retention_in_days = 30
 
   tags = {
-    Name = "web-log-group"
+    Name = "cb-log-group"
   }
 }
 
 resource "aws_cloudwatch_log_stream" "cb_log_stream" {
-  name           = "web-log-stream"
+  name           = "cb-log-stream"
   log_group_name = aws_cloudwatch_log_group.cb_log_group.name
 }
