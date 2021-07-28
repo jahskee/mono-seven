1e5ad96bda4690d69a6ab392da309d23c5205886
jahskee <jahskee@yahoo.com>
2021-07-28 05:53:36 -0400

    update

--- infra/pulumi/web/index.ts
+++ infra/pulumi/web/index.ts
@@ -1,21 +1,22 @@
 import * as awsx from "@pulumi/awsx";
 
-const cluster = new awsx.ecs.Cluster("nextjs-blog");
+const cluster = new awsx.ecs.Cluster("nextapp");
 
 const alb = new awsx.elasticloadbalancingv2.ApplicationLoadBalancer(
-    "nextjs-blog--lb", { external: true, securityGroups: cluster.securityGroups });
+    "nextapp--lb", { external: true, securityGroups: cluster.securityGroups });
 
 const atg = alb.createTargetGroup(
-    "nextjs-blog--tg", { port: 3000, protocol: "HTTP", deregistrationDelay: 0 });
+    "nextapp--tg", { port: 3000, protocol: "HTTP", deregistrationDelay: 0 });
 
 const web = atg.createListener("web", { port: 80 });
 
+const image = "458831896152.dkr.ecr.us-east-1.amazonaws.com/nextapp:latest";
 const appService = new awsx.ecs.FargateService("app-svc", {
     cluster,
     taskDefinitionArgs: {
         containers: {
-            nextjs_blog: {
-                image: "458831896152.dkr.ecr.us-east-1.amazonaws.com/nextapp:latest",
+            nextapp: {
+                image,
                 cpu: 1024,
                 memory: 2048,
                 portMappings: [ web ],
