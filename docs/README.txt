facd437b5471754ad928ef9581d849aca1fbcf1c
jahskee <jahskee@yahoo.com>
2021-08-06 09:18:20 -0400

    add apollo client 3

---
+++ docs/README.txt
@@ -0,0 +1,3 @@
+brew install gh
+gh auth login
+gh repo clone
--- rn_app/src/App.tsx
+++ rn_app/src/App.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function HomeScreen({ navigation }) {
   return (
@@ -23,14 +30,24 @@ function DetailsScreen({ navigation }) {
 
 const Stack = createStackNavigator();
 
+function LogoTitle() {
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
